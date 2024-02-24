7aa36b483f75aee227cd34aaad47856469ae04df
Ebtisam Alshehri <109087333+sambatsy@users.noreply.github.com>
2024-02-24 04:14:49 +0300

    Create transformation.py

---
+++ dataset/transformation.py
@@ -0,0 +1,5 @@
+portrait_dataset = Portrait(img_dir='/kaggle/input/art-portraits/Portraits_v2', transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
